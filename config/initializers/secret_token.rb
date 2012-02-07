4311c74ddba6c76fa7a19e23335589e7b751207e
Lanbo Chen <chenlanbo1988@gmail.com>
2012-02-06 19:51:04 -0500

    On branch master Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cse532SocialNetwork::Application.config.secret_token = '96319ca902862aa7e35560951d8045e796cf4f3e09a3d1533bf171ee23f6676882694a26fc6f8ac5c292b5c6cfdd56a1cdaccc55ffa109244b705b3458bddd3e'
