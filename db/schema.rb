cdb04e900696ca7cfc04b922d86885252937e45e
Lanbo Chen <chenlanbo1988@gmail.com>
2012-03-02 10:45:07 -0500

    On branch beta Changes to be committed: 	add circle_member to db 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120302153947) do
+ActiveRecord::Schema.define(:version => 20120302154402) do
+
+  create_table "circle_members", :force => true do |t|
+    t.integer  "circle_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "circles", :force => true do |t|
     t.string   "name"
