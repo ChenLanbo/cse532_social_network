d66a59269ee036acc171f9c63108c3adc1ff1589
Lanbo Chen <chenlanbo1988@gmail.com>
2012-03-06 01:23:26 -0500

    On branch beta Changes to be committed: 	modified:   app/controllers/friend_requests_controller.rb 	new file:   app/views/friend_requests/create.js.erb 	modified:   app/views/users/_user.html.haml

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -47,8 +47,8 @@ class FriendRequestsController < ApplicationController
 
     respond_to do |format|
       if @friend_request.save
-				# format.html { redirect_to users_path, :notice => 'Friend request was successfully sent.' }
 				format.html { redirect_to users_path, :notice => 'Friend request was successfully sent.' }
+				format.js
         format.json { render :json => @friend_request, :status => :created, :location => @friend_request }
       else
         format.html { render :action => "new" }
