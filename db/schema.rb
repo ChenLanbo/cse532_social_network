54a57e1685e06b7d7515a52a5c93e3f58c2243a5
Lanbo Chen <chenlanbo1988@gmail.com>
2012-03-09 01:47:54 -0500

    On branch beta Changes to be committed: 	add advertise to db 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120307163446) do
+ActiveRecord::Schema.define(:version => 20120309064633) do
+
+  create_table "advertisements", :force => true do |t|
+    t.string   "type"
+    t.string   "company"
+    t.string   "item_name"
+    t.decimal  "unit_price"
+    t.integer  "quantity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "circle_members", :force => true do |t|
     t.integer  "circle_id"
