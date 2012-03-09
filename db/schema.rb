562ba83b88d658eced97d895e0ac86d3f31ad2b0
Lanbo Chen <chenlanbo1988@gmail.com>
2012-03-09 02:10:59 -0500

    On branch beta Changes to be committed: 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120309064633) do
+ActiveRecord::Schema.define(:version => 20120309071017) do
 
   create_table "advertisements", :force => true do |t|
     t.string   "type"
@@ -105,6 +105,14 @@ ActiveRecord::Schema.define(:version => 20120309064633) do
     t.integer  "user_id"
     t.integer  "page_id"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sales", :force => true do |t|
+    t.integer  "advertisement_id"
+    t.integer  "user_id"
+    t.integer  "quantity"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
