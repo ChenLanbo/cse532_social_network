fecefc8c7af573a0432d611283de19218ea2f0b5
Lanbo Chen <chenlanbo1988@gmail.com>
2012-03-09 21:27:50 -0500

    Revert "On branch beta"
    This reverts commit aadd33cec18cf9b4d1649a67be755ffe13da86c8.
    
    Conflicts:
    
    	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 # It's strongly recommended to check this file into your version control system.
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20120309071017) do
+=======
+ActiveRecord::Schema.define(:version => 20120310021541) do
+>>>>>>> parent of aadd33c... On branch beta
 =======
 ActiveRecord::Schema.define(:version => 20120310021541) do
 >>>>>>> parent of aadd33c... On branch beta
