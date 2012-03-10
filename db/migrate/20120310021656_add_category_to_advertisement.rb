343dd0c5ef5f5de6fb0af5dc4eac62c1caadf222
Lanbo Chen <chenlanbo1988@gmail.com>
2012-03-09 21:25:10 -0500

    On branch beta

--- db/migrate/20120310021656_add_category_to_advertisement.rb
+++
@@ -1,4 +0,0 @@
-class AddCategoryToAdvertisement < ActiveRecord::Migration
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120310021656) do
+ActiveRecord::Schema.define(:version => 20120309071017) do
 
   create_table "advertisements", :force => true do |t|
     t.string   "type"
